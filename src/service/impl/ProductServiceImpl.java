8bfc6ce985388f9588a473c3fb081b2ea8f194e5
leonelborgesdev <leonel03t@gmail.com>
2024-01-30 08:26:51 -0400

    productServiceImpl

--- src/service/impl/ProductServiceImpl.java
+++ src/service/impl/ProductServiceImpl.java
@@ -1,4 +1,25 @@
 package service.impl;
 
-public class ProductServiceImpl {
+import entities.Product;
+import service.ProductService;
+import service.ShoppingCart;
+
+public class ProductServiceImpl implements ProductService {
+    private ShoppingCart cart;
+
+    public ProductServiceImpl(ShoppingCart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public void addProductToCart(Product product, int quantity) {
+        for (int i = 0; i < quantity; i++){
+            cart.addProduct(product);
+        }
+    }
+
+    @Override
+    public int getAllProductsFormCart() {
+        return cart.getTotalProducts();
+    }
 }
